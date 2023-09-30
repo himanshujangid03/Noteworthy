193cc1522836467b77e7603f73db4fdea952ca23
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 00:00:40 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     credentials: true,
     //origin: "http://localhost:3000",
-    origin: "https://noteworthy-silk.vercel.app",
+    origin: "https://noteworthy-zeta.vercel.app/",
   }),
 );
 
