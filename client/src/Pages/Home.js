e4aa9df35c8cefc2171d18cae1e6169ade005828
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 12:18:19 +0530

    deploy

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -7,7 +7,7 @@ import { usernameApi } from "../utils/api";
 
 function Home() {
   const data = useRouteLoaderData("loginData");
-  //const { name } = data;
+  const { name } = data;
 
   //const ctx = useContext(AuthContext);
   return (
