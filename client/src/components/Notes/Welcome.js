60f0b416a2e1f880cbb6aed61bee97f9e14f0da5
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 19:56:49 +0530

    deploy1

--- client/src/components/Notes/Welcome.js
+++ client/src/components/Notes/Welcome.js
@@ -6,15 +6,17 @@ function Welcome() {
   const name = data.name; */
   return (
     <>
-      <div className="welcome">
-        <div>
-          <h3>Welcome to Noteworthy Notes - Your Digital Notebook</h3>
-          <p>
-            Get organized, stay productive, and never forget a thing with our
-            Notes App. Whether you're jotting down ideas, making to-do lists, or
-            keeping important information at your fingertips, we've got you
-            covered.
-          </p>
+      <div className="welcome__div">
+        <div className="welcome">
+          <div>
+            <h3>Welcome to Noteworthy Notes - Your Digital Notebook</h3>
+            <p>
+              Get organized, stay productive, and never forget a thing with our
+              Notes App. Whether you're jotting down ideas, making to-do lists,
+              or keeping important information at your fingertips, we've got you
+              covered.
+            </p>
+          </div>
         </div>
       </div>
     </>
