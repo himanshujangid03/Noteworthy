8aa848f1d504c227b05125a67c6040e1f4b40e08
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 07:37:14 +0530

    deploy

--- server/controller/errorController.js
+++ server/controller/errorController.js
@@ -46,7 +46,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error("ERROR 💥", err.message);
+    console.error("ERROR 💥", err);
 
     // 2) Send generic message
     res.status(500).json({
