36b5459de5b741b5486a8ac3bf6bc98fc0111b16
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 12:36:58 +0530

    deploy

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -67,26 +67,20 @@ exports.login = async (req, res, next) => {
 
 exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    try {
-      // 1) verify token
-      const decoded = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET,
-      );
+    // 1) verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
 
-      // 2) Check if user still exists
-      const currentUser = await User.findById(decoded.id);
-      if (!currentUser) {
-        return next();
-      }
-
-      // THERE IS A LOGGED IN USER
-      req.user = currentUser;
-
-      return next();
-    } catch (err) {
+    // 2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
       return next();
     }
+
+    // THERE IS A LOGGED IN USER
+    req.user = currentUser;
   }
   next();
 };
