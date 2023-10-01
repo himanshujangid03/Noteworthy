1ab410d337f9ed0e712bc2e2a8ea734ef425b9f1
[himanshujangid03] <[himanshujangid516@gmail.com]>
2023-10-01 07:55:52 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -29,7 +29,10 @@ app.use(
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://noteworthy-zeta.vercel.app",
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
